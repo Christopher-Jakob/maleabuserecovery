d605c27b3c874a7da7600676cf1b3cfb9852c06c
Christopher Jakob <christopher.jakob@S03068-MBPR.local>
2018-12-05 14:23:55 -0600

    thing

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + 'dist/maleabuserecovery'));
+app.use(express.static(__dirname + '/dist/maleabuserecovery'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname +'dist/maleabuserecovery/index.html'));
+  res.sendFile(path.join(__dirname +'/dist/maleabuserecovery/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
